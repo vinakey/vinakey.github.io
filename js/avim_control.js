507e1527d5c6276d7db566f8b0eda4d5bce2274e
anh-ngn <anhngrx@gmail.com>
2024-01-13 23:41:30 +0700

    remove boostrap, fix layout display incorrectly on mobile devices

--- js/avim_control.js
+++ js/avim_control.js
@@ -1,20 +1,20 @@
 // Get all the radio buttons
-const radioButtons = document.querySelectorAll('.btn-group-toggle .btn input[type="radio"]');
+const radioButtons = document.querySelectorAll('.btn-group .avim-control-btn input[type="radio"]');
 
 // Add a change event listener to each radio button
 radioButtons.forEach(radioButton => {
     radioButton.addEventListener('change', function() {
         // Remove the 'active' class from all buttons
-        const buttons = document.querySelectorAll('.btn-group-toggle .btn');
-        buttons.forEach(btn => btn.classList.remove('active'));
+        const buttons = document.querySelectorAll('.btn-group .avim-control-btn');
+        buttons.forEach(btn => btn.classList.remove('is-active'));
 
         // Add the 'active' class to the parent button of the changed radio button
-        this.parentElement.classList.add('active');
+        this.parentElement.classList.add('is-active');
     });
 });
 
 // Manually trigger the change event for the checked radio button
-const checkedRadioButton = document.querySelector('.btn-group-toggle .btn input[type="radio"]:checked');
+const checkedRadioButton = document.querySelector('.btn-group .avim-control-btn input[type="radio"]:checked');
 if (checkedRadioButton) {
     checkedRadioButton.dispatchEvent(new Event('change'));
 }
