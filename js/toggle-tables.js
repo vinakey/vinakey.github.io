c65ac64a48698a9bc4d26e111e849051b4222721
Anh Nguyen <anhngw@gmail.com>
2024-01-15 01:36:11 +0700

    remove window.onload in toggle-table.js

--- js/toggle-tables.js
+++ js/toggle-tables.js
@@ -1,23 +1,22 @@
 function toggleTables(checkbox) {
-    var tables = document.querySelectorAll('.table-box');
+    var tables = document.querySelectorAll(".table-box");
     for (var i = 0; i < tables.length; i++) {
-        tables[i].style.display = checkbox.checked ? 'block' : 'none';
+        tables[i].style.display = checkbox.checked ? "block" : "none";
     }
-    localStorage.setItem('toggleTablesCheckbox', checkbox.checked);
+    localStorage.setItem("toggleTablesCheckbox", checkbox.checked);
 }
 
-window.onload = function() {
-    var checkbox = document.getElementById('toggleTablesCheckbox');
-    var checked = localStorage.getItem('toggleTablesCheckbox');
-    if (checked === null) {
-        checkbox.checked = true;
-        toggleTables(checkbox);
-    }
-    else if (checked === 'true') {
-        checkbox.checked = true;
-        toggleTables(checkbox);
-    } else {
-        checkbox.checked = false;
-        toggleTables(checkbox);
-    }
-};
+// window.onload = function() {
+var checkbox = document.getElementById("toggleTablesCheckbox");
+var checked = localStorage.getItem("toggleTablesCheckbox");
+if (checked === null) {
+    checkbox.checked = true;
+    toggleTables(checkbox);
+} else if (checked === "true") {
+    checkbox.checked = true;
+    toggleTables(checkbox);
+} else {
+    checkbox.checked = false;
+    toggleTables(checkbox);
+}
+// };
