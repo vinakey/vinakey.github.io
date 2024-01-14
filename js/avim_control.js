9d9a9151f9dbabe3478fc74bb5edd96e808e9d96
anh-ngn <anhngrx@gmail.com>
2024-01-14 22:03:30 +0700

    Update table row classes and add row highlighting functionality

--- js/avim_control.js
+++ js/avim_control.js
@@ -1,6 +1,26 @@
 // Get all the radio buttons
 const radioButtons = document.querySelectorAll('.btn-group .avim-control-btn input[type="radio"]');
 
+function highlightRows() {
+    var method = document.querySelector('input[name="AVIMMethod"]:checked').id;
+    var rows = document.querySelectorAll('tr[class^="row"]');
+    console.log(method);
+    for (var i = 0; i < rows.length; i++) {
+        rows[i].classList.remove('is-selected');
+    }
+    if (method === 'avim_auto') {
+        document.querySelectorAll('.row1, .row2').forEach(function(row) {
+            row.classList.add('is-selected');
+        });
+    } else if (method === 'avim_telex') {
+        document.querySelector('.row1').classList.add('is-selected');
+    } else if (method === 'avim_vni') {
+        document.querySelector('.row2').classList.add('is-selected');
+    } else if (method === 'avim_viqr') {
+        document.querySelector('.row3').classList.add('is-selected');
+    }
+}
+
 // Add a change event listener to each radio button
 radioButtons.forEach(radioButton => {
     radioButton.addEventListener('change', function() {
@@ -10,6 +30,7 @@ radioButtons.forEach(radioButton => {
 
         // Add the 'active' class to the parent button of the changed radio button
         this.parentElement.classList.add('is-dark');
+        highlightRows();
     });
 });
 
